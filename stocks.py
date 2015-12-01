3dde8cc98e4ef00889244e4ba9d18089981cd0cc
Brian Wilson <brianwilson1001@hotmail.com>
2015-12-01 14:14:54 +0000

    Move use of since to correct method

--- stocks.py
+++ stocks.py
@@ -16,24 +16,24 @@ trades = []
 class Stock(object):
     def __init__(self, stock, *args, **kwargs):
         if stock[1].lower() not in ['common', 'preferred']:
-
+            raise ValueError("Trade type must be one of 'common' or 'preferred'")
         super(Stock, self).__init__()
         (self.symbol, self.type, self.last_dividend, self.fixed_dividend,
             self.par_value = stocks)
         self.formulae = Formulae
 
     def calculate_dividend_yield(self):
-        if self.type == 'Common':
+        if self.type.lower() == 'common':
             return self.formulae.common_dividend_yield(
                 dividend, ticker
             )
-        elif self.type == 'Preferred':
+        elif self.type.lower() == 'preferred':
             return self.formulae.preferred_dividend_yield(
                 dividend, par, ticker
             )
 
-    def calculate_pe_ratio(self):
-        return self.formulae.pe_ratio(dividend, ticker)
+    def calculate_pe_ratio(self, dividend, ticker_price):
+        return self.formulae.pe_ratio(dividend, ticker_price)
 
     def record_trade(self, quantity, trade_type, price):
         if not isinstance(quantity, (int, long)):
@@ -51,15 +51,19 @@ class Stock(object):
         ]
         trades.append(trade)
 
-    def calculate_stock_price(self, trades):
-        return self.formulae.stock_price(trades)
-
-    def calculate_all_share_index(self, since=None):
+    def calculate_stock_price(self, since=None):
         if since is not None and not isinstance(since, datetime):
             raise TypeError('Since must be a datetime.')
 
-        prices = []
+        calculation_trades = []
         for trade in trades:
             if since is None or trade[0] >= since:
-                prices.append(trade[3])
+                calculation_trades.append(trade)
+
+        return self.formulae.stock_price(calculation_trades)
+
+    def calculate_all_share_index(self):
+        prices = []
+        for trade in trades:
+            prices.append(trade[3])
         return self.formulae.geometric_mean(prices)
